cd36ecb6411f916406df7711c082b5b61cc40f5f
Artem Riabchych <artemskyart@gmail.com>
2023-04-25 19:04:03 +0300

    add-styles

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./User.module.css";
 import PropTypes from "prop-types";
+import bgrdImg from "../../images/bgrdImg.svg";
 // import { TiThumbsUp, TiThumbsDown } from 'react-icons/ti';
 // import { formatDateToNow } from '../../helpers/formatDateToNow';
 import { Button } from "../Button/Button";
@@ -8,10 +9,11 @@ import { Button } from "../Button/Button";
 export const User = ({ followers, tweets, user, avatar, id, updateUser }) => {
   return (
     <li className={styles.card}>
+      <img className={styles.bgrd} src={bgrdImg} alt="image" />
       <img className={styles.avatar} src={avatar} alt={user} />
       <div className={styles.cardWrapper}>
-        <p>{tweets} TWEETS</p>
-        <p>{followers} FOLLOWERS</p>
+        <p className={styles.tweets}>{tweets} TWEETS</p>
+        <p className={styles.followers}>{followers} FOLLOWERS</p>
         <div className={styles.buttonBox}>
           <Button updateUser={updateUser} counter={followers} id={id}></Button>
         </div>
@@ -26,4 +28,5 @@ User.propTypes = {
   avatar: PropTypes.string.isRequired,
   tweets: PropTypes.number.isRequired,
   followers: PropTypes.number.isRequired,
+  updateUser: PropTypes.func.isRequired
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       container: {
         center: true,
         padding: {
-          DEFAULT: '1.5rem',
+          DEFAULT: "1.5rem",
         },
+      },
+      colors: {
+        accentColor: "#5736A3",
+        mainTextColor: "#EBD8FF",
+      },
+      width: {
+        380: "380px",
+        62: "62px"
+      },
+      height: {
+        460: "460px",
+        62: "62px"
+      },
+      translate: {
+        50: "-50%",
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [],
 };
