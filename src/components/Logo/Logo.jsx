ec91b51747db53f7e4756bc934a79a5c0d5e8cc6
Artem Riabchych <artemskyart@gmail.com>
2023-04-27 09:22:21 +0300

    fix-bugs

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,22 @@
 import React from "react";
-import logo from "../../images/hansel.svg"
+import { BsTwitter } from "react-icons/Bs";
+import { IconContext } from "react-icons";
+import logo from "../../images/hansel.svg";
 
 export const Logo = () => {
   return (
     <>
+      <IconContext.Provider
+        value={{
+          color: "#42A5F5",
+          className: "global-class-name",
+          size: "45px",
+        }}
+      >
+        <div>
+          <BsTwitter />
+        </div>
+      </IconContext.Provider>
       <img src={logo} alt="Logo" />
     </>
   );
